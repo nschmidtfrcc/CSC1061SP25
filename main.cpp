e4f7b82edab673172b99efc05e44ce787d3ef1a7
Chitrayoum <165188168+Chitrayoum@users.noreply.github.com>
2025-05-01 11:56:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 void PopulateCustomers(Customer customers[]);
 void displayCustomerList(Customer customers[],int customerCount);
-void linkCustomerToTheCar();
+void linkCustomersToCars(Customer customers[], int custCount,
+                         Car      soldCars[],  int carCount); 
 
 /*     displayCarInfo           --Isaiah Fite--
 Parameters: The function expects a list of cars
